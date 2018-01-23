65d190157a811667a886f32db6e9d8593887c7e9
brv00 <35671444+brv00@users.noreply.github.com>
2018-01-23 09:38:58 +0900

    Create permutations.scm

---
+++ permutations.scm
@@ -0,0 +1,9 @@
+(define (interleave x xs)
+  `((,x . ,xs) . ,(if (null? xs) '()
+		    (map (lambda (lis) (consh(car xs) lis))
+			 (interleave x (cdr xs))))))
+
+(define (permutations xs)
+  (if (null? xs) '(())
+    (apply append (map (lambda (lis) (interleave (car xs) lis))
+		       (permutations (cdr xs))))))
